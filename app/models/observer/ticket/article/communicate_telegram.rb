3043dddf7acad203646da1ecb0a2647c14c9f96d
Martin Edenhofer <me@edenhofer.de>
2017-02-03 00:16:01 +0100

    Init version of outbound communication.

--- app/models/observer/ticket/article/communicate_telegram.rb
+++ app/models/observer/ticket/article/communicate_telegram.rb
@@ -8,17 +8,13 @@ class Observer::Ticket::Article::CommunicateTelegram < ActiveRecord::Observer
     # return if we run import mode
     return if Setting.get('import_mode')
 
-    # only do send email if article got created via application_server (e. g. not
-    # if article and sender type is set via *.postmaster)
-    return if ApplicationHandleInfo.current.split('.')[1] == 'postmaster'
-
     # if sender is customer, do not communicate
     return if !record.sender_id
     sender = Ticket::Article::Sender.lookup(id: record.sender_id)
     return if sender.nil?
     return if sender['name'] == 'Customer'
 
-    # only apply on tweets
+    # only apply on telegram messages
     return if !record.type_id
     type = Ticket::Article::Type.lookup(id: record.type_id)
     return if type['name'] !~ /\Atelegram/i
